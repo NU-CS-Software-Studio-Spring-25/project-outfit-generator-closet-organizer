e10b3628dc9cc0135fae63fc681109cf47d1b9ba
aregaslanyan <151097104+aregaslanyan@users.noreply.github.com>
2025-04-30 17:54:50 -0500

    preparing to branch from main, updates to add new items

--- app/controllers/clothings_controller.rb
+++ app/controllers/clothings_controller.rb
@@ -6,4 +6,27 @@ class ClothingsController < ApplicationController
       @clothings = Clothing.all
     end
   end
+
+  def new
+    @clothing = Clothing.new
+  end
+  
+  def create
+    @clothing = Clothing.new(clothing_params)
+    if @clothing.save
+      redirect_to clothings_path, notice: "Clothing item added!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  
+  def clothing_params
+    params.require(:clothing).permit(:name, :brand, :category)
+  end
+  
 end
+
+
+
