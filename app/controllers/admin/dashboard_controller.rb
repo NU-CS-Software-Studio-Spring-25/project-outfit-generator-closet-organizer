5d7cdbcb168bdcd2c87de890c6e8c828aee31791
natalie3737 <natalieh3737@gmail.com>
2025-05-22 23:32:56 -0500

    Admin - Most Hidden Items
    Allows Admin to see in the Admin Dashboard what items are most frequently hidden by users.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,10 +3,20 @@ class Admin::DashboardController < ApplicationController
   before_action :require_admin
 
   def index
-    # Admin-only data can be set here
     @user_count = User.count
     @clothing_count = Clothing.count
+    @admin_items_count = Clothing.where(user_id: nil).count
     @recent_users = User.order(created_at: :desc).limit(5)
+
+    hidden_counts = HiddenClothing
+      .group(:clothing_id)
+      .order('count_all DESC')
+      .limit(5)
+      .count
+
+    @most_hidden = Clothing.where(id: hidden_counts.keys).index_by(&:id).transform_values do |clothing|
+      [clothing, hidden_counts[clothing.id]]
+    end.values
   end
 
   private
