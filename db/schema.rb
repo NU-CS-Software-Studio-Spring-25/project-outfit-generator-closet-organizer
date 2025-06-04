7b2811c9ed5e79c6fd67e246d232f3c99fe0610e
aregaslanyan <151097104+aregaslanyan@users.noreply.github.com>
2025-06-04 15:52:48 -0500

    changed button colors for accesibility

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_26_181436) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_01_212933) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -48,9 +48,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_26_181436) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
     t.string "tags"
     t.string "article"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_clothings_on_user_id"
   end
 
