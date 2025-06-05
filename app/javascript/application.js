27f404990b015fdb3eb12812e14fbee9b3061104
crystalsychen <crystal.chen586@gmail.com>
2025-06-05 10:24:29 -0500

    Fix UJS and redirect

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,4 +3,5 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
 import "./validate_new_clothing_form"
+import Rails from "@rails/ujs"
 Rails.start()
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,7 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.0/lib/assets/compiled/rails-ujs.js"
+pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
 pin "validate_new_clothing_form"
 
 
